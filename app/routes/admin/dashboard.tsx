04ef4d186880d16a0d9040e4060abc714eb66376
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-05-06 00:08:53 -0400

    Add files via upload
    Updated both StatsCard.tsx and TripCard.tsx along with dashboard.tsx

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,7 +1,10 @@
 import {Header, StatsCard, TripCard} from "../../../components";
+import {allTrips, dashboardStats, user} from "~/constants/";
+const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
 
 const Dashboard = () => {
-    const user = { name: 'Zach'};
+
+
 
     return (
         <main className="dashboard wrapper">
@@ -10,8 +13,47 @@ const Dashboard = () => {
                 description="Track activity, trends and popular destinations in real time"
             />
 
-            <StatsCard />
-            <TripCard />
+            <section className="flex flex-col gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+                    <StatsCard
+                        headerTitle="Total Users"
+                        total={dashboardStats.totalUsers}
+                        currentMonthCount={dashboardStats.usersJoined.currentMonth}
+                        lastMonthCount={dashboardStats.usersJoined.lastMonth}
+                    />
+                    <StatsCard
+                        headerTitle="Total Trips"
+                        total={dashboardStats.totalTrips}
+                        currentMonthCount={dashboardStats.tripsCreated.currentMonth}
+                        lastMonthCount={dashboardStats.tripsCreated.lastMonth}
+                    />
+                    <StatsCard
+                        headerTitle="Active Users"
+                        total={dashboardStats.userRole.total}
+                        currentMonthCount={dashboardStats.userRole.currentMonth}
+                        lastMonthCount={dashboardStats.userRole.lastMonth}
+                    />
+                </div>
+            </section>
+            <section className="container">
+                <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+
+                <div className='trip-grid'>
+                    {allTrips.slice(0, 4).map(({ id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
+                        <TripCard
+                            key={id}
+                            id={id.toString()}
+                            name={name!}
+                            imageUrl={imageUrls[0]}
+                            location={itinerary?.[0]?.location ?? ''}
+                            tags={tags}
+                            price={estimatedPrice!}
+                        />
+                    ))}
+                </div>
+            </section>
+
+
         </main>
     )
 }
